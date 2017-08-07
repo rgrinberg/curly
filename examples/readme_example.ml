951ee4ab0445519f7958d805b6b3ae9a2a6d15d0
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-08-07 10:18:09 -0400

    build readme example

---
+++ examples/readme_example.ml
@@ -0,0 +1,9 @@
+
+let () =
+  match Curly.(run (Request.make ~url:"https://opam.ocaml.org" ~meth:`GET ())) with
+  | Ok x ->
+    Format.printf "status: %d\n" x.Curly.Response.code;
+    Format.printf "headers: %a\n" Curly.Header.pp x.Curly.Response.headers;
+    Format.printf "body: %s\n" x.Curly.Response.body
+  | Error e ->
+    Format.printf "Failed: %a" Curly.Error.pp e
