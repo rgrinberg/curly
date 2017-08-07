d47adf455545246e00cbd55ee9f77ac925164131
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-08-07 05:22:57 -0400

    Initial commit

---
+++ src/curly.ml
@@ -0,0 +1,191 @@
+module Result = struct
+  include Result
+  let (>>=)
+    : type a b e. (a, e) result -> (a -> (b, e) result) -> (b, e) result
+    = fun r f ->
+      match r with
+      | Ok x -> f x
+      | (Error _) as e -> e
+end
+
+open Result
+
+module Header = struct
+  type t = (string * string) list
+
+  let empty = []
+
+  let to_cmd t =
+    t
+    |> List.map (fun (k, v) -> ["-H"; Printf.sprintf "%s: %s" k v])
+    |> List.concat
+
+  let pp fmt t =
+    Format.pp_print_list
+      ~pp_sep:Format.pp_print_newline
+      (fun fmt (k ,v) -> Format.fprintf fmt "%s: %s\n" k v)
+      fmt t
+end
+
+module Response = struct
+  type t = Http.response =
+    { code: int
+    ; headers: Header.t
+    ; body: string
+    }
+
+  let default =
+    { code = 0
+    ; headers = []
+    ; body = "" }
+
+  let of_stdout s =
+    let lexbuf = Lexing.from_string s in
+    try Ok (Http.response default lexbuf)
+    with e -> Error e
+
+  let pp fmt t =
+    Format.fprintf fmt "{code=%d;@ headers=%a;@ body=\"%s\"}"
+      t.code Header.pp t.headers t.body
+end
+
+module Process_result = struct
+  type t =
+    { status: Unix.process_status
+    ; stderr: string
+    ; stdout: string }
+
+  let pp_process_status fmt = function
+    | Unix.WEXITED n -> Format.fprintf fmt "Exit code %d" n
+    | Unix.WSIGNALED n -> Format.fprintf fmt "Signal %d" n
+    | Unix.WSTOPPED n -> Format.fprintf fmt "Stopped %d" n
+
+  let pp fmt t =
+    Format.fprintf fmt "{status=%a;@ stderr=\"%s\";@ stdout=\"%s\"}"
+      pp_process_status t.status t.stderr t.stdout
+end
+
+module Error = struct
+  type t =
+    | Invalid_request of string
+    | Bad_exit of Process_result.t
+    | Failed_to_read_response of exn * Process_result.t
+    | Exn of exn
+
+  let pp fmt = function
+    | Bad_exit p ->
+      Format.fprintf fmt "Non 0 exit code %a@.%a"
+        Process_result.pp_process_status p.Process_result.status
+        Process_result.pp p
+    | Failed_to_read_response (e, _)  ->
+      Format.fprintf fmt "Couldn't read response:@ %s" (Printexc.to_string e)
+    | Invalid_request r -> Format.fprintf fmt "Invalid request: %s" r
+    | Exn e -> Format.fprintf fmt "Exception: %s" (Printexc.to_string e)
+end
+
+module Request = struct
+  type t =
+    { meth: string
+    ; url: string
+    ; headers: Header.t
+    ; body: string
+    }
+
+  let make ?(headers=Header.empty) ?(body="") ~url ~meth =
+    { meth
+    ; url
+    ; headers
+    ; body }
+
+  let has_body t = String.length t.body > 0 
+
+  let validate t =
+    if has_body t && List.mem t.meth ["GET"; "HEAD"] then
+      Error (Error.Invalid_request "No body is allowed with GET/HEAD methods")
+    else
+      Ok t
+
+  let to_cmd_args t =
+    List.concat
+      [ ["-X"; t.meth]
+      ; Header.to_cmd t.headers
+      ; [t.url]
+      ; (if has_body t then
+           ["--data-binary"; "@-"]
+         else
+           [])
+      ]
+
+  let pp fmt t =
+    Format.fprintf fmt "{ meth=%s;@ url=\"%s\";@ headers=\"%a\";@ body=\"%s\"}"
+      t.meth t.url Header.pp t.headers t.body
+end
+
+let result_of_process_result t =
+  match t.Process_result.status with
+  | Unix.WEXITED 0 -> Ok t
+  | _ -> Error (Error.Bad_exit  t)
+
+let run prog args stdin_str =
+  let (stdout, stdin, stderr) =
+    let prog =
+      prog :: (List.map Filename.quote args)
+      |> String.concat " " in
+    Unix.open_process_full prog [||] in
+  Printf.eprintf "writing body of length %d\n%!" (String.length stdin_str);
+  output_string stdin stdin_str;
+  begin
+    try close_out stdin;
+    with _ -> ()
+  end;
+  let stdout_fd = Unix.descr_of_in_channel stdout in
+  let stderr_fd = Unix.descr_of_in_channel stderr in
+  let (in_buf, err_buf) = Buffer.(create 128, create 128) in
+  let read_buf_len = 512 in
+  let read_buf = Bytes.create read_buf_len in
+  let input ch =
+    match input ch read_buf 0 read_buf_len with
+    | 0 -> Error `Eof
+    | s -> Ok s in
+  let rec loop = function
+    | [] -> ()
+    | read_list ->
+      let can_read, _, _ = Unix.select read_list [] [] 1.0 in
+      List.fold_left (fun to_remove fh ->
+          let (rr, buf) =
+            if fh = stderr_fd then (
+              (input stderr, err_buf)
+            ) else (
+              (input stdout, in_buf)
+            ) in
+          begin match rr with
+            | Ok len ->
+              Buffer.add_subbytes buf read_buf 0 len;
+              fh :: to_remove
+            | Error `Eof ->
+              to_remove
+          end
+        ) [] can_read
+      |> loop
+  in
+  ignore (loop [ stdout_fd ; stderr_fd ]);
+  let status = Unix.close_process_full (stdout, stdin, stderr) in
+  { Process_result.
+    status
+  ; stdout = Buffer.contents in_buf
+  ; stderr = Buffer.contents err_buf
+  }
+
+let run ?(exe="curl") ?(args=[]) req =
+  Request.validate req >>= fun req ->
+  let args = "-si" :: (Request.to_cmd_args req) @ args in
+  let res =
+    try
+      result_of_process_result (run exe args req.Request.body)
+    with e ->
+      Error (Error.Exn e)
+  in
+  res >>= fun res ->
+  match Response.of_stdout res.Process_result.stdout with
+  | Ok r -> Ok r
+  | Error e -> Error (Error.Failed_to_read_response (e, res))
---
+++ src/curly.mli
@@ -0,0 +1,56 @@
+module Header : sig
+  type t = (string * string) list
+
+  val pp : Format.formatter -> t -> unit
+end
+
+module Response : sig
+  type t =
+    { code: int
+    ; headers: Header.t
+    ; body: string
+    }
+
+  val pp : Format.formatter -> t -> unit
+end
+
+module Request : sig
+  type t =
+    { meth: string
+    ; url: string
+    ; headers: Header.t
+    ; body: string
+    }
+
+  val make : ?headers:Header.t -> ?body:string -> url:string -> meth:string -> t
+
+  val to_cmd_args : t -> string list
+
+  val pp : Format.formatter -> t -> unit
+end
+
+module Process_result : sig
+  type t =
+    { status: Unix.process_status
+    ; stderr: string
+    ; stdout: string
+    }
+
+  val pp : Format.formatter -> t -> unit
+end
+
+module Error : sig
+  type t =
+    | Invalid_request of string
+    | Bad_exit of Process_result.t
+    | Failed_to_read_response of exn * Process_result.t
+    | Exn of exn
+
+  val pp : Format.formatter -> t -> unit
+end
+
+val run
+  : ?exe:string
+  -> ?args:string list
+  -> Request.t
+  -> (Response.t, Error.t) Result.result
