8674423b254671f354584135fcb9e4dafd8c0aa7
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-08-11 05:52:20 -0400

    Add method specific functions
    POST/GET/PUT/DELETE

--- examples/readme_example.ml
+++ examples/readme_example.ml
@@ -1,6 +1,6 @@
 
 let () =
-  match Curly.(run (Request.make ~url:"https://opam.ocaml.org" ~meth:`GET ())) with
+  match Curly.(request (Request.make ~url:"https://opam.ocaml.org" ~meth:`GET ())) with
   | Ok x ->
     Format.printf "status: %d\n" x.Curly.Response.code;
     Format.printf "headers: %a\n" Curly.Header.pp x.Curly.Response.headers;
